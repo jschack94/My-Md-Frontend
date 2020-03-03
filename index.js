bade8bce280dcdf0e401a9048225c0294cebe7fd
Jonathan <github email address>
2020-03-03 14:21:59 -0600

    work on doctor crud

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-//  IMPORTANT COMMENTS ************
-// in order to test the doctor homescreen, uncomment the fetchAppointments function call at bottom of file
-//  IMPORTANT COMMENTS ************
-// feel free to add more comments here!!! *****
-
-
-//  defined variables
+//variables
 
 APPOINTMENTS_ENDPOINT = "http://localhost:3000/appointments/"
+DOCTORS_ENDPOINT = "http://localhost:3000/doctors/"
 
-const body = document.querySelector("body")
 const appointmentList = document.querySelector(".appointment-list")
 const appDetailPanel = document.querySelector(".appointment-detail-panel")
-const doctorList = document.querySelector(".doctor-list")
-//  defined functions
-
-const renderDoctorHomeScreen = (event) => {
-  event.preventDefault()
-  const clicked = event.target
-  const doctorEmail = clicked.children[2].value
-}
-
-const loginScreen = () => {
-  const loginDiv = document.createElement("div")
-
-  body.innerHTML = `<h1 id="myMDLogo">myMD</h1><br><img style="float: right; margin-right: 100px;" class="medical-image" src="https://images.squarespace-cdn.com/content/v1/5908027c20099e374ad3d70e/1498497433363-9FIJ7FA1O2O1OMU760YE/ke17ZwdGBToddI8pDm48kEIuZxI6W46qNPE4tOwAgJl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k6sq9GEl9ZUDkp1sRKcAyLcGm_zFFSj8V81weFb6OmoAJ4fht0OgyKA20Hd2KoDYQ/symbol-of-caduceus.jpg?format=2500w" alt="medical-symbol"><div class="login" id="login">
-    <form class="login-form" action="index.html" method="post">
-      <label for="login-form">Please Enter Email to Login or Create New Account</label><br>
-      <input class="login-email" type="text" name="email" value="">
-      <input class="login-submit" type="submit" name="Submit" value="Submit">
-    </form>
-  </div>`
-
-  const loginForm = document.querySelector(".login-form")
-
-  loginForm.addEventListener('submit', renderDoctorHomeScreen)
-
-}
+const doctorContainer = document.querySelector(".doctor-container")
 
 
 const fetchAppointments = () => {
@@ -43,10 +13,6 @@ const fetchAppointments = () => {
         .then(resp => resp.json())
         .then(appointments => renderAppointments(appointments))
         .catch(err => renderErrors(err))
-}
-
-const renderErrors = (err) => {
-  appointmentList.innerHTML = `<h1>The following errors prevented the data from fetching. ${err}. Make sure rails server is running.`
 }
 
 const renderAppointments = (appointments) => {
@@ -87,9 +53,36 @@ const renderOneAppointment = (appointment) => {
   appDetailPanel.innerHTML = appt_detail
 }
 
+const fetchDoctors = () => {
+  fetch(DOCTORS_ENDPOINT)
+      .then(resp => resp.json())
+      .then(doctors => renderDoctors(doctors))
 
+}
+
+const renderDoctors = (doctors) => {
+  doctors.forEach(doctor => {
+  
+    const doctor_detail = `<div><h1><strong><span style="text-decoration: underline;">${doctor.full_name}</span></strong></span></h1><img src="${doctor.image}" alt="doctor photo">
+    <h2>Specialty: ${doctor.specialty}</h2>
+    <h3>Residency: ${doctor.residency}</h3>
+    <h3>Bio: ${doctor.bio}</h3>
+    <h3>Email: ${doctor.email}</h3>
+    <button data-doctor-id="${doctor.id}" id="edit-bio">Edit Doctor Bio</button>
+    </div>`
     
-    
+
+    doctorContainer.innerHTML += doctor_detail
+
+  })
+}
+
+
+  
+
+
+
+
 //  event listeners
 appointmentList.addEventListener('click', renderDetailedAppointment)
 
@@ -99,9 +92,5 @@ appointmentList.addEventListener('click', renderDetailedAppointment)
 
 
 //  invoked functions
-// fetchAppointments()
-<<<<<<< HEAD
+//fetchAppointments()
 fetchDoctors()
-=======
-loginScreen()
->>>>>>> a4fc6f828cc8ad3a3baa0a48213c88773283e789
